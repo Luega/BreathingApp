e656f6615c5502e07e7ec0fa0aa0f16ac9a65328
luega <lucamartinelli.developer@gmail.com>
2023-10-23 18:34:59 +0200

    created function to start animation

--- app/components/BreathingAnimation.tsx
+++ app/components/BreathingAnimation.tsx
@@ -5,7 +5,8 @@ import classes from "../style/breathingAnimation.module.css"
 
 const BreathingAnimation = () => {
   const { state, setState } = useBreathingAppContext();
-  const [breathingPhase, setBreathingPhase] = useState<string>("");
+  const [breathingPhase, setBreathingPhase] = useState<string>("Click to start");
+  const [startAnimation, setStartAnimation] = useState<boolean>(false);
 
   const gsapContainer = useRef(null);
   const breathingTl = useRef<GSAPTimeline>();
@@ -13,27 +14,32 @@ const BreathingAnimation = () => {
   const loops = Math.round(state.exerciseTime / (state.inhaleTime * 4)) - 1;
 
   useLayoutEffect(() => {
-    const ctx = gsap.context(() => {
-      breathingTl.current = gsap.timeline(
-        {
-          repeat: loops > 0 ? loops : 0
-        }
-      )
-        .add(() => { setBreathingPhase("IN") })
-        .to("#yellow_circle", { scale: 3, duration: state.inhaleTime, ease: "none" })
-        .add(() => { setBreathingPhase("APNEA") })
-        .to("#yellow_circle", { scale: 3, duration: state.inhaleTime, ease: "none" })
-        .add(() => { setBreathingPhase("OUT") })
-        .to("#yellow_circle", { scale: 1, duration: state.inhaleTime, ease: "none" })
-        .add(() => { setBreathingPhase("APNEA") })
-        .to("#yellow_circle", { scale: 1, duration: state.inhaleTime, ease: "none" })
+    if (startAnimation) {
+      const ctx = gsap.context(() => {
+        breathingTl.current = gsap.timeline(
+          {
+            repeat: loops > 0 ? loops : 0
+          }
+        )
+          .add(() => { setBreathingPhase("IN") })
+          .to("#yellow_circle", { scale: 3, duration: state.inhaleTime, ease: "none" })
+          .add(() => { setBreathingPhase("APNEA") })
+          .to("#yellow_circle", { scale: 3, duration: state.inhaleTime, ease: "none" })
+          .add(() => { setBreathingPhase("OUT") })
+          .to("#yellow_circle", { scale: 1, duration: state.inhaleTime, ease: "none" })
+          .add(() => { setBreathingPhase("APNEA") })
+          .to("#yellow_circle", { scale: 1, duration: state.inhaleTime, ease: "none" })
 
-    }, gsapContainer);
+      }, gsapContainer);
 
-    return () => ctx.revert();
+      return () => ctx.revert();
+    }
+  }, [startAnimation]);
 
-  }, []);
-
+  const restartHandler = () => {
+    setStartAnimation(!startAnimation);
+    setBreathingPhase("Click to start")
+  }
 
   return (
     <div ref={gsapContainer}>
@@ -43,7 +49,7 @@ const BreathingAnimation = () => {
           <div className={`${classes.white_circle} w-[250px] h-[250px] sm:w-[375px] sm:h-[375px] lg:w-[500px] lg:h-[500px] 2xl:w-[625px] 2xl:h-[625px]`}>
             <div className={`${classes.white_circle} w-[200px] h-[200px] sm:w-[300px] sm:h-[300px] lg:w-[400px] lg:h-[400px] 2xl:w-[500px] 2xl:h-[500px]`}>
               <div className={`${classes.white_circle} w-[150px] h-[150px] sm:w-[225px] sm:h-[225px] lg:w-[300px] lg:h-[300px] 2xl:w-[375px] 2xl:h-[375px]`}>
-                <div id="yellow_circle" className={`${classes.yellow_circle} w-[100px] h-[100px] sm:w-[150px] sm:h-[150px] lg:w-[200px] lg:h-[200px] 2xl:w-[250px] 2xl:h-[250px]`}>
+                <div onClick={() => restartHandler()} id="yellow_circle" className={`${classes.yellow_circle} w-[100px] h-[100px] sm:w-[150px] sm:h-[150px] lg:w-[200px] lg:h-[200px] 2xl:w-[250px] 2xl:h-[250px] cursor-pointer`}>
                   {breathingPhase}
                 </div>
               </div>
