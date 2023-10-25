c8407878a90e647980899a1e9ea1eff4af3a2421
luega <lucamartinelli.developer@gmail.com>
2023-10-25 16:41:29 +0200

    changed exercises in DB

--- app/DB/db.ts
+++ app/DB/db.ts
@@ -3,21 +3,34 @@ import { Exercise } from "../utils/types";
 const db: Exercise[] = [
   {
     name: "Symmetric breathing",
-    inspiratoryApnea: boolean,
-    expiratoryApnea: boolean,
-    simmetry: boolean,
+    inhale: 1,
+    exhale: 1,
+    inspiratoryApnea: 0,
+    expiratoryApnea: 0,
     tags: ["Awareness"],
   },
   {
     name: "Asymmetric breathing",
+    inhale: 1,
+    exhale: 2,
+    inspiratoryApnea: 0,
+    expiratoryApnea: 0,
     tags: ["Relax"],
   },
   {
     name: "Triangular breathing",
+    inhale: 1,
+    exhale: 1,
+    inspiratoryApnea: 0,
+    expiratoryApnea: 1,
     tags: ["Relax", "Health"],
   },
   {
     name: "Box breathing",
+    inhale: 1,
+    exhale: 1,
+    inspiratoryApnea: 1,
+    expiratoryApnea: 1,
     tags: ["Focus", "Relax"],
   },
 ];
