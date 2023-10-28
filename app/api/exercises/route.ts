79a281e7ac057ec60a35c97545c2c9166f984e4c
luega <lucamartinelli.developer@gmail.com>
2023-10-28 11:53:37 +0200

    fixed CORS

--- app/api/exercises/route.ts
+++ app/api/exercises/route.ts
@@ -7,9 +7,13 @@ export async function GET(_req: Request, _res: Response) {
     return new Response(JSON.stringify(exercises), {
       status: 200,
       headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Access-Control-Allow-Credentials":
+          process.env.ACCESS_CONTROL_ALLOW_CREDENTIALS!,
+        "Access-Control-Allow-Origin": process.env.ACCESS_CONTROL_ALLOW_ORIGIN!,
+        "Access-Control-Allow-Methods":
+          process.env.ACCESS_CONTROL_ALLOW_METHODS!,
+        "Access-Control-Allow-Headers":
+          process.env.ACCESS_CONTROL_ALLOW_HEADERS!,
       },
     });
   } catch (error: any) {
