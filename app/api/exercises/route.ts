e3d0a214e5ea5afcc1299a36a62874cf902bba30
luega <lucamartinelli.developer@gmail.com>
2023-10-28 16:55:24 +0200

    trying solve cors issue

--- app/api/exercises/route.ts
+++ app/api/exercises/route.ts
@@ -4,7 +4,9 @@ import { Exercise } from "@/app/utils/types";
 export async function GET(_req: Request, res: Response) {
   try {
     const exercises: Exercise[] = await getAllExercises();
-    return new Response(JSON.stringify(exercises), { status: 200 });
+    return new Response(JSON.stringify(exercises), {
+      status: 200,
+    });
   } catch (error: any) {
     return new Response(error, { status: 500 });
   }
