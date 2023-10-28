3d49961c89db67acc41fc945cfbce66e5d376e29
luega <lucamartinelli.developer@gmail.com>
2023-10-28 16:23:54 +0200

    trying solve cors issue

--- app/api/exercises/route.ts
+++ app/api/exercises/route.ts
@@ -4,16 +4,7 @@ import { Exercise } from "@/app/utils/types";
 export async function GET(_req: Request, _res: Response) {
   try {
     const exercises: Exercise[] = await getAllExercises();
-    return new Response(JSON.stringify(exercises), {
-      status: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET,PUT,POST,PATCH,DELETE,OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Content-Type": "application/json",
-        "Cache-Control": "no-cache",
-      },
-    });
+    return new Response(JSON.stringify(exercises), { status: 200 });
   } catch (error: any) {
     return new Response(error, { status: 500 });
   }
