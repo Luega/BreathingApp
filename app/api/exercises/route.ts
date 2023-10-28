628b066a972a221777e2f4c9554dc109c3d49c36
luega <lucamartinelli.developer@gmail.com>
2023-10-28 18:07:33 +0200

    trying solve cors issue

--- app/api/exercises/route.ts
+++ app/api/exercises/route.ts
@@ -1,20 +1,19 @@
 import { getAllExercises } from "@/app/mongoBD/controllers/ExerciseController";
 import { Exercise } from "@/app/utils/types";
 
-export async function GET(_req: Request, res: Response) {
+export async function GET(req: Request, _res: Response) {
   try {
     const exercises: Exercise[] = await getAllExercises();
+    const allowedOrigin = req.headers.get("origin");
     return new Response(JSON.stringify(exercises), {
       status: 200,
-      headers: [
-        ["Access-Control-Allow-Origin", "*"],
-        ["Access-Control-Allow-Credentials", "true"],
-        ["Access-Control-Allow-Methods", "GET,DELETE,PATCH,POST,PUT"],
-        [
-          "Access-Control-Allow-Headers",
-          "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-        ],
-      ],
+      headers: {
+        "Access-Control-Allow-Origin": allowedOrigin || "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers":
+          "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
+        "Access-Control-Max-Age": "86400",
+      },
     });
   } catch (error: any) {
     return new Response(error, { status: 500 });
