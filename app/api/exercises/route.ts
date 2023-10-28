9894d5ac3813863ef64c6b206f5661463058c329
luega <lucamartinelli.developer@gmail.com>
2023-10-28 17:47:34 +0200

    trying solve cors issue

--- app/api/exercises/route.ts
+++ app/api/exercises/route.ts
@@ -6,6 +6,15 @@ export async function GET(_req: Request, res: Response) {
     const exercises: Exercise[] = await getAllExercises();
     return new Response(JSON.stringify(exercises), {
       status: 200,
+      headers: [
+        ["Access-Control-Allow-Origin", "*"],
+        ["Access-Control-Allow-Credentials", "true"],
+        ["Access-Control-Allow-Methods", "GET,DELETE,PATCH,POST,PUT"],
+        [
+          "Access-Control-Allow-Headers",
+          "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+        ],
+      ],
     });
   } catch (error: any) {
     return new Response(error, { status: 500 });
