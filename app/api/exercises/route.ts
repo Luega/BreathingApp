fadef99c0f99c4b03f769681315f831fca66116b
luega <lucamartinelli.developer@gmail.com>
2023-10-28 11:16:15 +0200

    fixed cors

--- app/api/exercises/route.ts
+++ app/api/exercises/route.ts
@@ -4,7 +4,15 @@ import { Exercise } from "@/app/utils/types";
 export async function GET(_req: Request, _res: Response) {
   try {
     const exercises: Exercise[] = await getAllExercises();
-    return new Response(JSON.stringify(exercises), { status: 200 });
+    return new Response(JSON.stringify(exercises), {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin":
+          "https://breathing-oa9xifzjj-luega.vercel.app/",
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
+    });
   } catch (error: any) {
     return new Response(error, { status: 500 });
   }
